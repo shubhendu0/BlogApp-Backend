807c1bd4291c68cc7e9c101e7f16a0cfae99021e
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-03-16 23:06:10 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 const Connection = async () => {
     const URL = `mongodb+srv://ShubhenduBora:titan123@cluster0.szpoh1n.mongodb.net/blogapp`;
     try {
-        await mongoose.connect(URL)
+        await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
     } 
     catch (error) {
